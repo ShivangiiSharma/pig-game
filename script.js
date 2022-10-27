2f3deaf646711c0d68c36e0e87e1556139cc41f5
ShivangiiSharma <developershivangii@gmail.com>
2022-10-27 12:43:27 +0530

    modified file

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ console.log('hey!');
 for (let i = 0; i < 10; i++) {
   console.log(`line ${i}`);
 }
+
+// one
+
+function sum1(abc) {
+  console.log(abc + 2);
+}
+// two
+
+const sum2 = function (abc) {
+  console.log(abc + 2);
+};
+
+// three
+
+const sum3 = abc => console.log(abc + 2);
+
+sum1(2);
+sum2(4);
+sum3(6);
