c1a92bed641b29a0603d804797f68f8957cf7464
ShivangiiSharma <developershivangii@gmail.com>
2022-10-28 09:38:44 +0530

    modified file

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // rolling dice functionality
 btnRoll.addEventListener('click', function () {
